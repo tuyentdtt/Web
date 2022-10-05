5a2f09a70580a7b75a7a01d0e0b4971c413bbe13
Tuyentdtt <tuyentdgcs18721@fpt.edu.vn>
2022-10-06 02:07:29 +0700

    Added product detail

---
+++ WebClient/Controllers/ProductDetailController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebClient.Controllers
+{
+    public class ProductDetailController : Controller
+    {
+        [Route("/productdetail")]
+        // GET: ProductDetailController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ProductDetailController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ProductDetailController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductDetailController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductDetailController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductDetailController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductDetailController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductDetailController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
